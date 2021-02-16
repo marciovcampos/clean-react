9be8dc6a4757dc52a54cee5db4286b9d1c732e32
Márcio Vinícius <marcio.campos@outlook.com>
2021-02-16 18:45:24 -0300

    refactor: change HttpPostClient to receive an object

--- src/data/protocols/http/http-post-client.ts
+++ src/data/protocols/http/http-post-client.ts
@@ -1,4 +1,7 @@
-export interface HttpPostClient {
-  post (url: string): Promise<void>
+export type HttpPostParams = {
+  url: string
 }
 
+export interface HttpPostClient {
+  post (params: HttpPostParams): Promise<void>
+}
